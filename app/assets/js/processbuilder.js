4d7b0e6377e1636ddabd5b473258d06cc3140ade
go-tiger <dev.gotiger@gmail.com>
2025-01-02 03:03:35 +0900

    v2.0.2

--- app/assets/js/processbuilder.js
+++ app/assets/js/processbuilder.js
@@ -521,7 +521,7 @@ class ProcessBuilder {
                             val = args[i].replace(argDiscovery, tempNativePath)
                             break
                         case 'launcher_name':
-                            val = args[i].replace(argDiscovery, 'Sample-Launcher')
+                            val = args[i].replace(argDiscovery, 'moolchoco-Launcher')
                             break
                         case 'launcher_version':
                             val = args[i].replace(argDiscovery, this.launcherVersion)
